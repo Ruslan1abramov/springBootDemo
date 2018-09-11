d5e033c28860ee67493086ccdb425ad4d4f63a7a
ruslan1abramov <ruslan1abramov@gmail.com>
2018-09-11 20:26:25 +0300

    swagger

--- src/main/java/com/example/demo/ItemController.java
+++ src/main/java/com/example/demo/ItemController.java
@@ -4,6 +4,10 @@ import java.net.URISyntaxException;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.hateoas.Resource;
 import org.springframework.hateoas.Resources;
 import org.springframework.http.ResponseEntity;
@@ -19,6 +23,7 @@ import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
 import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
 
 @RestController
+@Api(value="onlinestore", description="Operations pertaining to items in Online Store")
 class ItemController {
 
     private final ItemRepository repository;
@@ -30,7 +35,12 @@ class ItemController {
     }
 
     // Aggregate root
-
+    @ApiOperation(value = "View a list of all available products",response = Resources.class)
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Successfully retrieved items"),
+            @ApiResponse(code = 404, message = "The resource you were trying to reach is not found")
+    }
+    )
     @GetMapping("/items")
     Resources<Resource<Item>> all() {
 
@@ -42,6 +52,7 @@ class ItemController {
                 linkTo(methodOn(ItemController.class).all()).withSelfRel());
     }
 
+    @ApiOperation(value = "Adding a new item",produces = "application/json")
     @PostMapping("/items")
     ResponseEntity<?> newItem(@RequestBody Item newItem) throws URISyntaxException {
 
@@ -53,7 +64,12 @@ class ItemController {
     }
 
     // Single item
-
+    @ApiOperation(value = "View item by id",response = Resource.class)
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Successfully retrieved item"),
+            @ApiResponse(code = 404, message = "The resource you were trying to reach is not found")
+    }
+    )
     @GetMapping("/items/{id}")
     Resource<Item> one(@PathVariable Long id) {
 
@@ -63,6 +79,7 @@ class ItemController {
         return assembler.toResource(item);
     }
 
+    @ApiOperation(value = "Updating specific item amount")
     @PutMapping("/items/{id}")
     ResponseEntity<?> replaceItem(@RequestBody AmountToChange amountToChange, @PathVariable Long id)  throws URISyntaxException{
 
@@ -83,6 +100,7 @@ class ItemController {
                 .body(resource);
     }
 
+    @ApiOperation(value = "Delete item")
     @DeleteMapping("/items/{id}")
     ResponseEntity<?> deleteItem(@PathVariable Long id) {
 
