183f7db7dbf2590ce5687a7a1f31445faf42b81d
ruslan1abramov <ruslan1abramov@gmail.com>
2018-09-14 16:16:10 +0300

    docker

--- src/main/java/com/example/demo/ItemNotFoundException.java
+++ src/main/java/com/example/demo/ItemNotFoundException.java
@@ -3,6 +3,6 @@ package com.example.demo;
 public class ItemNotFoundException extends RuntimeException {
 
     ItemNotFoundException(Long id) {
-        super("Could not find employee " + id);
+        super("Could not find item " + id);
     }
 }
